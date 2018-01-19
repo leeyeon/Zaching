4a3bca19b78c301012624709ba52d1cdb50cb7e7
leeyeon <bbqgoood@nate.com>
2018-01-19 19:14:26 +0900

    연희: Update SQL

--- Zaching/SQL(script)/initialize.sql
+++ Zaching/SQL(script)/initialize.sql
@@ -1,23 +1,13 @@
 
-DROP TABLE transaction;
-DROP TABLE product;
-DROP TABLE users;
-DROP TABLE inventory;
-
-DROP SEQUENCE seq_user_user_id;
-DROP SEQUENCE seq_product_prod_no;
-DROP SEQUENCE seq_transaction_tran_no;
-DROP SEQUENCE seq_inventory_inven_no;
-
 SELECT seq_product_prod_no.currval FROM DUAL;
 
 CREATE SEQUENCE seq_user_user_id		 	INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE seq_product_prod_no		 	INCREMENT BY 1 START WITH 10000;
-CREATE SEQUENCE seq_transaction_tran_no	 INCREMENT BY 1 START WITH 10000;
-CREATE SEQUENCE seq_inventory_inven_no  INCREMENT BY 1 START WITH 10000;
-CREATE SEQUENCE seq_cart_cart_no  INCREMENT BY 1 START WITH 10000;
+CREATE SEQUENCE seq_bob_bob_id		 	INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_participant_participant_id	 INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_fee_fee_id  INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_comment_comment_id  INCREMENT BY 1 START WITH 1;
 
-CREATE TABLE users ( 
+CREATE TABLE USERS ( 
 	user_id      		number	 NOT NULL,
 	email  	 VARCHAR2(50)	 NOT NULL,
 	password  		 VARCHAR2(16)	  NOT NULL,
@@ -50,184 +40,69 @@ CREATE TABLE users (
 	PRIMARY KEY(user_id)
 );
 
-//alter table users add(sns_id VARCHAR2(50));
-//alter table users add(sns_type VARCHAR2(20));
-alter table users add(profile_image VARCHAR2(200));
-
-
-CREATE TABLE product ( 
-	prod_no 						NUMBER 				NOT NULL,
-	prod_name 				VARCHAR2(100) 	NOT NULL,
-	prod_detail 				VARCHAR2(200),
-	manufacture_day		 VARCHAR2(8),
-	price 							NUMBER(10),
-	image_file 					VARCHAR2(100),
-	reg_date 					DATE,
-	amount					 NUMBER,
-	status					 CHAR(3),
-	PRIMARY KEY(prod_no)
+CREATE TABLE BOB (
+	bob_id numeric not null,
+	category_coe char(4) not null,
+	title varchar2(50) not null,
+	content varchar2(4000) not null,
+	created_date date not null,
+	background_image varchar2(100),
+	location_name varchar2(30),
+	latitude numeric,
+	longtitude numeric,
+	time date,
+	limit_num numeric,
+	fee numeric,
+	fee_date date,
+	status char(2) default 'Y' not null,
+	writer_id numeric not null,
+	PRIMARY KEY(bob_id),
+	FOREIGN KEY(writer_id) REFERENCES USERS(user_ID)
 );
 
-alter table product add(amount NUMBER);
-//ALTER TABLE product DROP COLUMN amount;
-
-//alter table product add(beforeAmount NUMBER);
-alter table product add(status NUMBER); // 판매 유무
-alter table transaction add(amount NUMBER);
-alter table transaction add(status NUMBER);
-
-UPDATE PRODUCT
-SET AMOUNT = (
-SELECT DISTINCT NVL2(TRAN_STATUS_CODE, 1,0)
-FROM TRANSACTION
-WHERE  PRODUCT.PROD_NO = TRANSACTION.PROD_NO(+));
-
-CREATE TABLE transaction ( 
-	tran_no 					NUMBER 			NOT NULL,
-	prod_no 					NUMBER(16)		NOT NULL REFERENCES product(prod_no),
-	buyer_id 				VARCHAR2(20)	NOT NULL REFERENCES users(user_id),
-	payment_option		 CHAR(3),
-	receiver_name 		 VARCHAR2(20),
-	receiver_phone		 VARCHAR2(14),
-	dlvy_addr 			 VARCHAR2(100),
-	dlvy_request 			VARCHAR2(100),
-	dlvy_date 				DATE,
-	tran_status_code	 CHAR(3),
-	order_date 			 DATE,
-	status				 CHAR(3),
-	PRIMARY KEY(tran_no)
+CREATE TABLE PARTICIPANT (
+	participant_id numeric not null,
+	participant_date date not null,
+	status char(2) not null,
+	bob_id numeric not null,
+	user_id numeric not null,
+	PRIMARY KEY(participant_id),
+	FOREIGN KEY(user_id) REFERENCES USERS(user_id),
+	FOREIGN KEY(bob_id) REFERENCES BOB(bob_ID)
 );
 
-================================================================
-재고관리 테이블
-================================================================
-
-CREATE TABLE inventory ( 
-	inven_no		 NUMBER NOT NULL,
-	tran_no  		 NUMBER 		NOT NULL REFERENCES transaction(tran_no),
-	amount 		 	NUMBER 		NOT NULL,
-	PRIMARY KEY (inven_no)
+CREATE TABLE FEE (
+	fee_id numeric not null,
+	paid_date date not null,
+	paid_fee numeric not null,
+	participant_id numeric not null,
+	PRIMARY KEY(fee_id),
+	FOREIGN KEY(participant_id) REFERENCES PARTICIPANT(participant_id)
 );
 
-================================================================
-장바구니 테이블
-================================================================
-CREATE TABLE cart (
-	cart_no 		NUMBER NOT NULL,
-	prod_no 		NUMBER NOT NULL REFERENCES product(prod_no),
-	user_id 		VARCHAR2(20)	 NOT NULL REFERENCES users(user_id),
-	cart_date 		DATE,
-	status 			CHAR(3),
-	PRIMARY KEY (cart_no)
+CREATE TABLE COMMENTS (
+	comment_id numeric not null,
+	content varchar2(1000) not null,
+	created_date date not null,
+	category_code varchar2(4) not null,
+	status char(2) default 'Y' not null,
+	room_id numeric not null,
+	user_id numeric not null,
+	FOREIGN KEY(user_id) REFERENCES USERS(user_id)
 );
 
-//alter table cart add(status CHAR(3));
-================================================================
-
-INSERT 
-INTO users ( user_id, user_name, password, role, ssn, cell_phone, addr, email, reg_date ) 
-VALUES ( 'admin', 'admin', '1234', 'admin', NULL, NULL, '서울시 서초구', 'admin@mvc.com',to_date('2012/01/14 10:48:43', 'YYYY/MM/DD HH24:MI:SS')); 
-
-INSERT 
-INTO users ( user_id, user_name, password, role, ssn, cell_phone, addr, email, reg_date ) 
-VALUES ( 'manager', 'manager', '1234', 'admin', NULL, NULL, NULL, 'manager@mvc.com', to_date('2012/01/14 10:48:43', 'YYYY/MM/DD HH24:MI:SS'));          
-
-INSERT INTO users 
-VALUES ( 'user01', 'SCOTT', '1111', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user02', 'SCOTT', '2222', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user03', 'SCOTT', '3333', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user04', 'SCOTT', '4444', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user05', 'SCOTT', '5555', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user06', 'SCOTT', '6666', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user07', 'SCOTT', '7777', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user08', 'SCOTT', '8888', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user09', 'SCOTT', '9999', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user10', 'SCOTT', '1010', 'user', NULL, NULL, NULL, NULL, sysdate); 
-
-INSERT INTO users 
-VALUES ( 'user11', 'SCOTT', '1111', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user12', 'SCOTT', '1212', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user13', 'SCOTT', '1313', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user14', 'SCOTT', '1414', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user15', 'SCOTT', '1515', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user16', 'SCOTT', '1616', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user17', 'SCOTT', '1717', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user18', 'SCOTT', '1818', 'user', NULL, NULL, NULL, NULL, sysdate);
-
-INSERT INTO users 
-VALUES ( 'user19', 'SCOTT', '1919', 'user', NULL, NULL, NULL, NULL, sysdate);
-           
-           
-insert into product values (seq_product_prod_no.nextval,'vaio vgn FS70B','소니 바이오 노트북 신동품','20120514',2000000, 'AHlbAAAAtBqyWAAA.jpg',to_date('2012/12/14 11:27:27', 'YYYY/MM/DD HH24:MI:SS'),3,'0');
-insert into product values (seq_product_prod_no.nextval,'자전거','자전거 좋아요~','20120514',10000, 'AHlbAAAAvetFNwAA.jpg',to_date('2012/11/14 10:48:43', 'YYYY/MM/DD HH24:MI:SS'),1,'0');
-insert into product values (seq_product_prod_no.nextval,'보르도','최고 디자인 신품','20120201',1170000, 'AHlbAAAAvewfegAB.jpg',to_date('2012/10/14 10:49:39', 'YYYY/MM/DD HH24:MI:SS'),1,'0');
-insert into product values (seq_product_prod_no.nextval,'보드세트','한시즌 밖에 안썼습니다. 눈물을 머금고 내놓음 ㅠ.ㅠ','20120217', 200000, 'AHlbAAAAve1WwgAC.jpg',to_date('2012/11/14 10:50:58', 'YYYY/MM/DD HH24:MI:SS'),1,'0');
-insert into product values (seq_product_prod_no.nextval,'인라인','좋아욥','20120819', 20000, 'AHlbAAAAve37LwAD.jpg',to_date('2012/11/14 10:51:40', 'YYYY/MM/DD HH24:MI:SS'),1,'0');
-insert into product values (seq_product_prod_no.nextval,'삼성센스 2G','sens 메모리 2Giga','20121121',800000, 'AHlbAAAAtBqyWAAA.jpg',to_date('2012/11/14 18:46:58', 'YYYY/MM/DD HH24:MI:SS'),2,'0');
-insert into product values (seq_product_prod_no.nextval,'연꽃','정원을 가꿔보세요','20121022',232300, 'AHlbAAAAtDPSiQAA.jpg',to_date('2012/11/15 17:39:01', 'YYYY/MM/DD HH24:MI:SS'),2,'0');
-insert into product values (seq_product_prod_no.nextval,'삼성센스','노트북','20120212',600000, 'AHlbAAAAug1vsgAA.jpg',to_date('2012/11/12 13:04:31', 'YYYY/MM/DD HH24:MI:SS'),3,'0');
+CREATE TABLE Voice ( 
+	voice_ID 		  NUMERIC			  NOT NULL,
+	category_code 		  VARCHAR2(4)		  NOT NULL,
+	title 			  VARCHAR2(50)		  NOT NULL,
+	background_image	  VARCHAR2(100), 
+	created_date 		  DATE			  NOT NULL,
+	song	 		  VARCHAR2(100), 
+	status 			  CHAR(2)			  DEFAULT'Y',  
+	user_ID 		  NUMERIC			  NOT NULL  REFERENCES users(user_ID),  
+	PRIMARY KEY(voice_ID)
+);
 
 
 commit;
 
-
-
-//== Page 처리을 위한 SQL 구성연습
-
-SELECT user_id , user_name , email
-FROM users
-ORDER BY user_id
-
-currentPage =2
-pageSize = 3   
-4 ~ 6
-
-SELECT inner_table. * ,  ROWNUM AS row_seq
-FROM (	SELECT user_id , user_name , email
-				FROM users
-				ORDER BY user_id ) inner_table
-WHERE ROWNUM <=6;	
-//==>           currentPage * paseSize
-
-
-SELECT * 
-FROM (	SELECT inner_table. * ,  ROWNUM AS row_seq
-				FROM (	SELECT user_id , user_name , email
-								FROM users
-								ORDER BY user_id ) inner_table
-				WHERE ROWNUM <=6 )
-WHERE row_seq BETWEEN 4 AND 6;
-
-//==> (currentPage-1) * paseSize+1           currentPage * paseSize
