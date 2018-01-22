0cb06823d618854647753a95ee9cc1e50c39f67d
bitcamp <bitcamp@bit>
2018-01-22 10:08:22 +0900

    newsfeedService commit

--- Zaching/src/main/java/com/zaching/service/newsfeed/NewsfeedService.java
+++ Zaching/src/main/java/com/zaching/service/newsfeed/NewsfeedService.java
@@ -1,5 +1,18 @@
 package com.zaching.service.newsfeed;
 
-public interface NewsfeedService {
+import java.util.List;
 
+import com.zaching.common.domain.Search;
+import com.zaching.service.domain.Newsfeed;
+
+public interface NewsfeedService {
+	public void addNewsfeed(Newsfeed newsfeed) throws Exception;
+	
+	public void deleteNewsfeed(int newsfeedID) throws Exception;
+	
+	public void updateNewsfeed(Newsfeed newsfeed) throws Exception;
+	
+	public void getNewsfeed(int newsfeedID) throws Exception;
+	
+	public List<Newsfeed> listNewsfeed(Search search) throws Exception;
 }
