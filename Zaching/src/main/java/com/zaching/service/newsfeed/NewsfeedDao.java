56cf9553030cd2fee4e7c703c1ee74d730b38715
bitcamp <bitcamp@bit>
2018-01-22 10:06:21 +0900

    newsfeedDao commit

--- Zaching/src/main/java/com/zaching/service/newsfeed/NewsfeedDao.java
+++ Zaching/src/main/java/com/zaching/service/newsfeed/NewsfeedDao.java
@@ -1,5 +1,20 @@
 package com.zaching.service.newsfeed;
 
-public interface NewsfeedDao {
+import java.util.Map;
 
+import com.zaching.common.domain.Search;
+import com.zaching.service.domain.Newsfeed;
+
+public interface NewsfeedDao {
+public void addNewsfeed(Newsfeed newsfeed) throws Exception;
+	
+	public void deleteNewsfeed(int newsfeedID) throws Exception;
+	
+	public void updateNewsfeed(Newsfeed newsfeed) throws Exception;
+	
+	public void getNewsfeed(int newsfeedID) throws Exception;
+	
+	public Map<String, Object> listNewsfeed(Search search) throws Exception;
+	
+	public int getCountLike(int newsfeedID) throws Exception; 
 }
