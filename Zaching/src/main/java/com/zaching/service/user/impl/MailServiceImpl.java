b325500ef9671c28983bf065bdad431fe432b2f0
hellorange79 <33915276+hellorange79@users.noreply.github.com>
2018-01-23 10:32:38 +0900

    정은:이미지저장

---
+++ Zaching/src/main/java/com/zaching/service/user/impl/MailServiceImpl.java
@@ -0,0 +1,51 @@
+package com.zaching.service.user.impl;
+
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import com.zaching.service.user.MailService;
+
+public class MailServiceImpl implements MailService {
+	
+	// org.springframework.mail.javamail.JavaMailSender
+    private JavaMailSender javaMailSender;
+ 
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+	@Override
+	public boolean send(String subject, String text, String from, String to, String filePath) {
+        // javax.mail.internet.MimeMessage
+        MimeMessage message = javaMailSender.createMimeMessage();
+ 
+        try {
+            // org.springframework.mail.javamail.MimeMessageHelper
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+ 
+            // 梅何 颇老 贸府
+            if (filePath != null) {
+                File file = new File(filePath);
+                if (file.exists()) {
+                    helper.addAttachment(file.getName(), new File(filePath));
+                }
+            }
+ 
+            javaMailSender.send(message);
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
--- Zaching/src/main/java/com/zaching/web/user/UserController.java
+++ Zaching/src/main/java/com/zaching/web/user/UserController.java
@@ -1,24 +1,84 @@
 
 package com.zaching.web.user;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.zaching.service.domain.User;
+import com.zaching.service.user.UserService;
 
 @Controller
 @RequestMapping("/user/*")
 public class UserController {
 
-	public UserController() {
-		// TODO Auto-generated constructor stub
-		System.out.println(this.getClass());
-	}
 	
-	@RequestMapping(value="test")
-	public String test() {
-		System.out.println("ㅇㅅㅇ");
+	///Field
+		@Autowired
+		@Qualifier("userServiceImpl")
+		private UserService userService;
+		//setter Method 구현 않음
+			
+		public UserController(){
+			System.out.println(this.getClass());
+		}
 		
-		return "redirect:/user/addProduct.jsp";
-	}
+		@Value("#{commonProperties['pageUnit']}")
+		int pageUnit;
+		@Value("#{commonProperties['pageSize']}")
+		int pageSize;
+		
+		
+		
+		
+		@RequestMapping( value="addUser", method=RequestMethod.GET )
+		public String addUser() throws Exception{
+		
+			System.out.println("/user/addUser : GET");
+			
+			return "redirect:/user/addUser.jsp";
+		}
+		
+		@RequestMapping( value="addUser", method=RequestMethod.POST )
+		public String addUser( @ModelAttribute("user") User user ) throws Exception {
+
+			System.out.println("/user/addUser : POST");
+			//Business Logic
+			userService.addUser(user);
+			
+			return "redirect:/user/login.jsp";
+		}
+		
+
+		@RequestMapping( value="getUser", method=RequestMethod.GET )
+		public String getUser( @RequestParam("userId") int userId , Model model ) throws Exception {
+			
+			System.out.println("/user/getUser : GET");
+			//Business Logic
+			User user = userService.getUser(userId);
+			// Model 과 View 연결
+			model.addAttribute("user", user);
+			
+			return "forward:/user/getUser.jsp";
+		}
 	
 	
 
