c11b75c516d2145060b79c6aa32743a39a9b1eec
hellorange79 <33915276+hellorange79@users.noreply.github.com>
2018-02-07 10:54:42 +0900

    정은: 오류 진짜 수정

--- Zaching/src/main/java/com/zaching/web/user/LoginController.java
+++ Zaching/src/main/java/com/zaching/web/user/LoginController.java
@@ -23,82 +23,95 @@ import com.zaching.service.user.UserService;
 @Controller
 @RequestMapping("/*")
 public class LoginController {
-	
+
 	/// Field
-		@Autowired
-		@Qualifier("commonServiceImpl")
-		private CommonService commonService;
-		
-		@Autowired
-		@Qualifier("userServiceImpl")
-		private UserService userService;
-		
-		@Autowired
-		@Qualifier("naverServiceImpl")
-		private NaverService naverService;
-		
-		public LoginController() {
-			System.out.println(this.getClass());
+	@Autowired
+	@Qualifier("commonServiceImpl")
+	private CommonService commonService;
+
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private UserService userService;
+
+	@Autowired
+	@Qualifier("naverServiceImpl")
+	private NaverService naverService;
+
+	public LoginController() {
+		System.out.println(this.getClass());
+	}
+
+	@Value("#{commonProperties['pageUnit']}")
+	int pageUnit;
+	@Value("#{commonProperties['pageSize']}")
+	int pageSize;
+
+	@RequestMapping(value = "kakaoLoginRequest", method = RequestMethod.GET)
+	public String kakaoLoginRequest() {
+
+		System.out.println("[ kakao Login Request!! ]");
+
+		return commonService.getAuthorizationUrl_login();
+
+	}
+
+	//
+	@RequestMapping(value = "kakaoLogin", method = RequestMethod.GET)
+	public String KakaoLogin(@RequestParam("code") String code, HttpSession session) throws Exception {
+
+		System.out.println("/kakaoLogin/code");
+
+		User user = commonService.getAceessToken2(code);
+		user = commonService.getUserInfo(user);
+
+		if (userService.login(user.getEmail()) == null) {
+
+			userService.addUser(user);
 		}
 
-		@Value("#{commonProperties['pageUnit']}")
-		int pageUnit;
-		@Value("#{commonProperties['pageSize']}")
-		int pageSize;
+		session.setAttribute("user", user);
+
+		System.out.println("session 저장정보===>" + session.getAttribute("user"));
+		System.out.println("이메일 ==> " + user.getEmail());
+		System.out.println("프로필 이미지 ==> " + user.getProfileImage());
+
+		return "forward:/index.jsp";
+	}
+
+	@RequestMapping(value = "naverLoginRequest", method = RequestMethod.GET)
+	public String naverLoginRequest(HttpSession session) {
+
+		System.out.println("[ Naver Login Request!! ]");
+
+		return naverService.getAuthorizationUrl_login(session);
+
+	}
+
+	@RequestMapping(value = "naverLogin", method = RequestMethod.GET)
+	public String naverLogin(@RequestParam("code") String code, @RequestParam("state") String state,
+			HttpSession session) throws Exception {
+
+		System.out.println("naverLogin/code받아서 토큰 요청하ㅣ");
+
+		User user = naverService.getAccessToken(session, code, state);
+		user = naverService.getUserProfile(user);
 		
+		System.out.println("code ===>"+code);
+		System.out.println("session ==>"+state);
+		System.out.println("session ==>"+session);
 		
-		@RequestMapping(value= "kakaoLoginRequest", method=RequestMethod.GET)
-		public String kakaoLoginRequest() {
-			
-			System.out.println("[ kakao Login Request!! ]");
-			
-			return commonService.getAuthorizationUrl_login();
-			
+		if (userService.login(user.getEmail()) == null) {
+
+			userService.addUser(user);
 		}
 
-		//
-		@RequestMapping(value="kakaoLogin",method=RequestMethod.GET)
-		public String KakaoLogin(@RequestParam("code") String code,
-									HttpSession session)throws Exception {
-			
-			System.out.println("/kakaoLogin/code");
-			
-			User user = commonService.getAceessToken2(code);
-			user = commonService.getUserInfo(user);
-			
-			
-			if(userService.login(user.getEmail()) == null) {
-				
-				userService.addUser(user);
-		}
-			
-			session.setAttribute("user", user);
-			
-			System.out.println("session 저장정보===>"+session.getAttribute("user"));
-			System.out.println("이메일 ==> "+user.getEmail());
-			System.out.println("프로필 이미지 ==> "+user.getProfileImage());
-			
-			
-			return "forward:/index.jsp";
-		}
-		
-		
-		@RequestMapping(value= "naverLoginRequest", method=RequestMethod.GET)
-		public String naverLoginRequest() {
-			
-			System.out.println("[ Naver Login Request!! ]");
-			
-			return naverService.getAuthorizationUrl_login();
-			
-		}
-		
-		
-		@RequestMapping(value="naverLogin", method=RequestMethod.GET)
-		public String facebookLogin(@RequestParam("code") String code,
-				HttpSession session)throws Exception {
-			
-			
-			return"";
-		}
+		session.setAttribute("user", user);
+
+		System.out.println("session 저장정보===>" + session.getAttribute("user"));
+		System.out.println("이메일 ==> " + user.getEmail());
+		System.out.println("프로필 이미지 ==> " + user.getProfileImage());
+
+		return "redirect:/index.jsp";
+	}
 
 }
