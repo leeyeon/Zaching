d462da17296fdf61b67d82aec030853112242936
bitcamp <bitcamp@bit>
2018-02-11 17:22:47 +0900

    상민 controller

--- Zaching/src/main/java/com/zaching/web/friend/FriendController.java
+++ Zaching/src/main/java/com/zaching/web/friend/FriendController.java
@@ -2,6 +2,8 @@ package com.zaching.web.friend;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.zaching.common.domain.Page;
 import com.zaching.common.domain.Search;
 import com.zaching.service.domain.Friend;
+import com.zaching.service.domain.User;
 import com.zaching.service.friend.FriendService;
 
 @Controller
@@ -49,7 +52,7 @@ public class FriendController {
 	}
 
 	@RequestMapping(value = "listFriend",method=RequestMethod.GET)
-	public String listFriend(@ModelAttribute("search") Search search, Model model) throws Exception {
+	public String listFriend(@ModelAttribute("search") Search search,HttpSession session, Model model) throws Exception {
 		System.out.println("friend/listFriend : GET");
 		
 		System.out.println("요기까지 왔습니다");
@@ -58,6 +61,11 @@ public class FriendController {
 			search.setCurrentPage(1);
 		}
 		search.setPageSize(pageSize);
+		
+		if (search.getSearchKeyword() == null) {
+			search.setSearchKeyword(((User)session.getAttribute("user")).getUserId()+"");
+			
+		}
 
 		Map<String, Object> map = friendService.listFriend(search);
 		System.out.println(search);
