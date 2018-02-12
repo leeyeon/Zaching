e7f64b6734819a63f2f21ce37edf54383a338b5c
bitcamp <bitcamp@bit>
2018-02-12 14:05:56 +0900

    지원 : 알림 끝

--- Zaching/src/main/java/com/zaching/web/notice/NoticeController.java
+++ Zaching/src/main/java/com/zaching/web/notice/NoticeController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.zaching.common.domain.Page;
 import com.zaching.common.domain.Search;
 import com.zaching.common.service.CommonService;
 import com.zaching.service.domain.Friend;
@@ -44,6 +45,13 @@ public class NoticeController {
 
 	private FriendService friendService;
 	
+	@Value("#{commonProperties['pageUnit']}")
+	// @Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+
+	@Value("#{commonProperties['pageSize']}")
+	// @Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
 
 
 	public NoticeController() {
@@ -53,34 +61,34 @@ public class NoticeController {
 	
 	
 	@RequestMapping(value = "makeNotice", method = RequestMethod.GET)
-	public String makeNotice(Model model, @ModelAttribute("notice") Notice notice, @ModelAttribute("search") Search search) throws Exception {
+	public String makeNotice(Model model, @ModelAttribute("notice") Notice notice,HttpSession session, @ModelAttribute("search") Search search) throws Exception {
 		
 		//각자 notice setting
+		if (search.getCurrentPage() == 0) {
+			search.setCurrentPage(1);
+		}
+		
+		search.setPageSize(pageSize);
 		
 		notice.setSenderId(9);
 		notice.setCategory("B00");
 		notice.setName("이연희");
 		notice.setBobId(3);
 		commonService.addNotice(notice);
-		
-		System.out.println(notice);
-		System.out.println("유저 아이디~~"+String.valueOf(notice.getSenderId()));
 		//search 키워드(userId)로 친구 목록 가지고 오기
 		//search.setSearchKeyword(String.valueOf(((User)session.getAttribute("user")).getUserId()));
 		search.setSearchKeyword(String.valueOf(notice.getSenderId()));
-		System.out.println(search);
 		Map<String, Object> map = friendService.listFriend(search);
-		System.out.println("칭구:"+map);
 		
 		for(int i=0; i< ((List)map.get("list")).size();i++) {
 			Friend friend = (Friend)((List)map.get("list")).get(i);
 			notice.setUserId(friend.getFriendId());
-			System.out.println("요기이이이이"+friend.getFriendId());
-			System.out.println("뀨"+notice);
+
 			commonService.addNoticeTarget(notice);
-			System.out.println("밍"+notice);
+
 		}
 		//여기까지
+		
 
 		
 		return "forward:/admin/test2.jsp";
