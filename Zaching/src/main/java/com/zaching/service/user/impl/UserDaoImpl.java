db247dfe2d25089e9e52de8f7839b4b0f466fe6b
zwon47 <deabang421@naver.com>
2018-02-14 14:59:56 +0900

    jinsu / commit

--- Zaching/src/main/java/com/zaching/service/user/impl/UserDaoImpl.java
+++ Zaching/src/main/java/com/zaching/service/user/impl/UserDaoImpl.java
@@ -132,5 +132,9 @@ public class UserDaoImpl implements UserDao{
 	public String getAccountToken(int userId) throws Exception {
 		return sqlSession.selectOne("UserMapper.getAccountToken", userId);
 	}
+	
+	public String getPWD(String id) throws Exception{
+		return sqlSession.selectOne("UserMapper.getPassword", id);
+	}
 
 }
